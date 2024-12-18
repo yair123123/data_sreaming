102c53ab330101a33bb809b49a15e4580b599951
Yair <ytykhn8@gmail.com>
2024-12-18 17:43:17 +0200

    add faust

--- app/faust_f.py
+++ app/faust_f.py
@@ -2,22 +2,24 @@ import os
 
 import faust
 from dotenv import load_dotenv
-from streaming_data import process
+from streaming_data import  process_mongo, process_neo4j
+
 load_dotenv(verbose=True)
 
-app_faust = faust.App(
+app = faust.App(
     "streaming",
     broker=os.getenv("BOOTSTRAP_SERVER"),
     value_serializer='json'
 )
 
-@app_faust.agent(os.getenv("TOPIC_PROCESS"))
+@app.agent(app.topic(os.getenv("TOPIC_PROCESS")))
 async def send_to_dbs(messages):
     async for message in messages:
-        topic_psql = app_faust.topic(os.getenv("TOPIC_CONSUME_MONGO"), message)
-        topic_mongo = app_faust.topic(os.getenv("TOPIC_CONSUME_NEO4J"), message)
+        print(type(message))
+        topic_mongo = app.topic(os.getenv("TOPIC_CONSUME_MONGO"))
+        # topic_neo4j = app.topic(os.getenv("TOPIC_CONSUME_NEO4J"))
         try:
-            await topic_mongo.send(value=message.value)
-            await topic_psql.send(value=process((message.value)))
+            await topic_mongo.send(value=process_mongo(message))
+            # await topic_neo4j.send(value=process_neo4j((message)))
         except Exception as e:
             print(e)
--- app/streaming_data.py
+++ app/streaming_data.py
@@ -1,17 +1,39 @@
-import json
-from typing import List, Any
+from typing import List, Dict
 
 import toolz as t
-from pandas import DataFrame
-from toolz.curried import partial
+
+a = {0: 'eventid', 1: 'year', 2: 'month', 3: 'day', 4: 'country', 5: 'city', 6: 'latitude', 7: 'longitude', 8: 'region',
+ 9: 'target_type', 10: 'target1', 11: 'target_nationality', 12: 'group_name', 13: 'group_name2', 14: 'attacktype1_txt',
+ 15: 'num_terrorists', 16: 'num_spread', 17: 'num_killed'}
+
+a = (['eventid', 'year', 'month', 'day', 'country', 'city', 'latitude',
+      'longitude', 'region', 'target_type', 'target1',
+      'target_nationality', 'group_name', 'group_name2', 'attacktype1_txt',
+      'num_terrorists', 'num_spread', 'num_killed'])
 
 
+def create_model(row: List[str]) -> Dict[str, Dict[str, str]]:
+    a = {k: v for k, v in enumerate(row)}
+    return {
+        "eventid": row[0],
+        "date": {"year": row[1], "month": row[2], "day": row[3]},
+        "location": {"country": row[4], "city": row[5], "region": row[8], "latitude": row[6], "longitude": row[7]},
+        "target": {"target_type": row[9], "target1_name": row[10], "target_nationality": row[11]},
+        "groups": [row[12], row[13]],
+        "attack": {"num_terrorists": row[15], "attack_type": row[14]},
+        "result": {"num_spread": row[16], "num_killed": row[17]},
+    }
 
 
-def process(list_of_rows) -> str:
-    list_of_rows: List[List[Any]] = json.loads(list_of_rows.value().decode('utf-8'))
-    df_data = DataFrame(list_of_rows)
+def process_neo4j(list_of_rows) -> str:
     return t.pipe(
-        df_data,
+        list_of_rows,
 
+    )
+
+
+def process_mongo(list_of_rows) -> str:
+    return t.pipe(
+        list_of_rows,
+        lambda x: [create_model(row) for row in x],
     )
