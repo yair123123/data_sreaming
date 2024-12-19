bf352a5ba3d2c96b03bdd08e1a35647bed7d24cc
DELL <ytykhn8@gmail.com>
2024-12-19 09:23:21 +0200

    add processing data

--- app/faust_f.py
+++ app/faust_f.py
@@ -1,8 +1,11 @@
+import asyncio
 import os
 
 import faust
 from dotenv import load_dotenv
-from streaming_data import  process_mongo, process_neo4j
+
+from app.streaming_data import process_neo4j
+from streaming_data import  process
 
 load_dotenv(verbose=True)
 
@@ -17,9 +20,21 @@ async def send_to_dbs(messages):
     async for message in messages:
         print(type(message))
         topic_mongo = app.topic(os.getenv("TOPIC_CONSUME_MONGO"))
-        # topic_neo4j = app.topic(os.getenv("TOPIC_CONSUME_NEO4J"))
+        topic_neo4j_target = app.topic(os.getenv("TOPIC_CONSUME_NEO4J_TARGET"))
+        topic_neo4j_attack = app.topic(os.getenv("TOPIC_CONSUME_NEO4J_ATTACK"))
+        topic_neo4j_group = app.topic(os.getenv("TOPIC_CONSUME_NEO4J_GROUP"))
+        topic_neo4j_region = app.topic(os.getenv("TOPIC_CONSUME_NEO4J_LOCATION"))
+        topic_neo4j_event = app.topic(os.getenv("TOPIC_CONSUME_NEO4J_EVENT"))
+        processed = process_neo4j(message)
         try:
-            await topic_mongo.send(value=process_mongo(message))
-            # await topic_neo4j.send(value=process_neo4j((message)))
+            tasks = [
+                topic_mongo.send(value=process(message)),
+                topic_neo4j_target.send(value=processed.get("target")),
+                topic_neo4j_attack.send(value=processed.get("attack")),
+                topic_neo4j_group.send(value=processed.get("group")),
+                topic_neo4j_region.send(value=processed.get("location")),
+                topic_neo4j_event.send(value=processed.get("event"))
+            ]
+            await asyncio.gather(*tasks)
         except Exception as e:
             print(e)
--- app/streaming_data.py
+++ app/streaming_data.py
@@ -1,19 +1,19 @@
 from typing import List, Dict
 
 import toolz as t
+from toolz import pluck, get_in
 
-a = {0: 'eventid', 1: 'year', 2: 'month', 3: 'day', 4: 'country', 5: 'city', 6: 'latitude', 7: 'longitude', 8: 'region',
- 9: 'target_type', 10: 'target1', 11: 'target_nationality', 12: 'group_name', 13: 'group_name2', 14: 'attacktype1_txt',
- 15: 'num_terrorists', 16: 'num_spread', 17: 'num_killed'}
 
-a = (['eventid', 'year', 'month', 'day', 'country', 'city', 'latitude',
-      'longitude', 'region', 'target_type', 'target1',
-      'target_nationality', 'group_name', 'group_name2', 'attacktype1_txt',
-      'num_terrorists', 'num_spread', 'num_killed'])
+def convert_event_to_save(event: Dict[str, Dict[str, str]]):
+    return {"eventid": event.get("event_id"),
+            **event.get("data"),
+            "country": get_in(["location", "country"], event),
+            "attack_type": get_in(["attack", "attack_type"], event),
+            "target_type": get_in([["target", "target_type"]], event),
+            "groups": event.get("groups")}
 
 
 def create_model(row: List[str]) -> Dict[str, Dict[str, str]]:
-    a = {k: v for k, v in enumerate(row)}
     return {
         "eventid": row[0],
         "date": {"year": row[1], "month": row[2], "day": row[3]},
@@ -25,15 +25,18 @@ def create_model(row: List[str]) -> Dict[str, Dict[str, str]]:
     }
 
 
-def process_neo4j(list_of_rows) -> str:
-    return t.pipe(
-        list_of_rows,
-
-    )
-
-
 def process_mongo(list_of_rows) -> str:
     return t.pipe(
         list_of_rows,
         lambda x: [create_model(row) for row in x],
     )
+
+
+def process_neo4j(message: List[Dict[str, Dict[str, str]]]):
+    return {
+        "location": set(pluck(["location"], message)),
+        "groups": set(pluck("groups", message)),
+        "attack_type": set(pluck(["attack", "attack_type"], message)),
+        "target_type": set(pluck(["target", "target_type"], message)),
+        "event": [convert_event_to_save(d) for d in message]
+    }
